54b41388b46ab5aa598a4a63306bb0db03d7d631
mkki <devmkki@gmail.com>
2025-06-28 00:34:30 +0900

    {#3} [feat] switch 추가

---
+++ src/components/ui/Switch/Switch.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames/bind';
+import styles from './Switch.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface SwitchProps {
+  label?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLButtonElement>) => void;
+}
+
+export const Switch = ({
+  label,
+  checked,
+  disabled,
+  onClick,
+  onKeyDown,
+}: SwitchProps) => {
+  return (
+    <button
+      type="button"
+      role="switch"
+      className={cx('button-switch')}
+      aria-checked={checked}
+      disabled={disabled}
+      onClick={onClick}
+      onKeyDown={onKeyDown}
+    >
+      <span className={cx('sr-only')}>{label}</span>
+    </button>
+  );
+};
---
+++ src/stories/Switch.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { Switch } from '@/components/ui/Switch/Switch';
+
+const meta = {
+  title: 'Example/Switch',
+  component: Switch,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Switch>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// 기본 예시들
+export const Unchecked: Story = {
+  args: {
+    label: 'option A',
+    checked: false,
+  },
+};
+
+export const Checked: Story = {
+  args: {
+    label: 'option B',
+    checked: true,
+  },
+};
