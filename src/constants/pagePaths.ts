5d326b5b19949e9184cd139f57e5cdd02a929875
mkki <devmkki@gmail.com>
2025-06-29 21:39:59 +0900

    [feat] 페이지 설정 기반 레이아웃 요소 렌더링

--- src/constants/pagePaths.ts
+++ src/constants/pagePaths.ts
@@ -1,13 +1,35 @@
+interface PageConfig {
+  header?: {
+    title?: string;
+    hasBackButton?: boolean;
+  };
+  showBottomNavigation?: boolean;
+}
+
+export type PagePath = keyof typeof PAGE_PATHS;
+
 export const PAGE_PATHS = {
+  ROOT: '/',
   STATUS: '/status',
   CHALLENGE: '/challenge',
   HISTORY: '/history',
   PROFILE: '/profile',
 } as const;
 
-export const MAIN_PAGE_PATHS = [
-  PAGE_PATHS.STATUS,
-  PAGE_PATHS.CHALLENGE,
-  PAGE_PATHS.HISTORY,
-  PAGE_PATHS.PROFILE,
-] as const;
+export const PAGE_CONFIG: Record<string, PageConfig> = {
+  [PAGE_PATHS.STATUS]: {
+    showBottomNavigation: true,
+  },
+  [PAGE_PATHS.CHALLENGE]: {
+    header: { title: '퀘스트' },
+    showBottomNavigation: true,
+  },
+  [PAGE_PATHS.HISTORY]: {
+    header: { title: '히스토리', hasBackButton: true },
+    showBottomNavigation: true,
+  },
+  [PAGE_PATHS.PROFILE]: {
+    header: { title: '마이페이지' },
+    showBottomNavigation: true,
+  },
+} as const;
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import { MAIN_PAGE_PATHS, PAGE_PATHS } from '@/constants/pagePaths';
+import { PAGE_CONFIG, PAGE_PATHS } from '@/constants/pagePaths';
+import { Header } from '@/components/ui/Header/Header';
 import { BottomNavigation } from '@/components/ui/BottomNavigation/BottomNavigation';
 
 import classNames from 'classnames/bind';
@@ -9,13 +10,14 @@ const cx = classNames.bind(styles);
 
 export const RootLayout: React.FC = () => {
   const location = useLocation();
+  const pathname = location.pathname;
 
   /**
    * [TODO] HOME 기준
    * 최초 유저 & 현재 진행 중인 '챌린지'가 없는 유저: [목표&챌린지]화면이 Home(Default)
    * (그외)현재 진행 중인 챌린지가 있는 유저: [상태창]이 Home(Default)
    */
-  const isRootPath = location.pathname === '/';
+  const isRootPath = pathname === PAGE_PATHS.ROOT;
   const hasActiveChallenge = false;
   const isFirstTimeUser = true;
 
@@ -23,16 +25,17 @@ export const RootLayout: React.FC = () => {
     return <Navigate to={PAGE_PATHS.CHALLENGE} replace />;
   }
 
-  const shouldShowBottomNavigation = MAIN_PAGE_PATHS.some(
-    (path) => path === location.pathname
-  );
+  const { header, showBottomNavigation } = PAGE_CONFIG[pathname];
 
   return (
     <div className={cx('wrapper')}>
+      {header && (
+        <Header title={header?.title} hasBackButton={header.hasBackButton} />
+      )}
       <main className={cx('main')}>
         <Outlet />
       </main>
-      {shouldShowBottomNavigation && <BottomNavigation />}
+      {showBottomNavigation && <BottomNavigation />}
     </div>
   );
 };
