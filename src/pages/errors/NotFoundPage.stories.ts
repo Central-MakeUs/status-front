34561d817427c9c16e87c8d072b8b8e5b4f8b286
mkki <devmkki@gmail.com>
2025-07-11 01:38:37 +0900

    #17 [feat] 글로벌 에러 페이지 스토리북 추가

---
+++ src/pages/errors/NotFoundPage.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { withRouter } from 'storybook-addon-react-router-v6';
+
+import { NotFoundPage } from '@/pages/errors/NotFoundPage';
+
+const meta = {
+  title: 'pages/errors',
+  component: NotFoundPage,
+  parameters: {
+    docs: {
+      description: {
+        story: '404 에러 페이지',
+      },
+    },
+  },
+  decorators: [withRouter],
+} satisfies Meta<typeof NotFoundPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const NotFound: Story = {};
