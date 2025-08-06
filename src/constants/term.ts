24c06ab772111434ba20436f73de8d45c0df6cbb
mkki <devmkki@gmail.com>
2025-08-07 01:50:32 +0900

    [feat] 마이 페이지에 앱 버전, 문의하기 추가

--- src/constants/term.ts
+++ src/constants/term.ts
@@ -1,4 +1,6 @@
 export const TERM_URL = {
   TERMS_OF_SERVICE: 'https://www.notion.so/242b86814a1180eeb5d0ee670aa26aa7',
   PRIVACY_POLICY: 'https://www.notion.so/244b86814a118059abf1c4995f3cd78a',
+  INQUIRY:
+    'https://splendid-fiction-653.notion.site/246b86814a11808b865cf3b4ee8ae533?source=copy_link',
 } as const;
