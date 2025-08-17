c36f7cdcdf06fadb759af8df7b68ef1abefb3ebd
mkki <devmkki@gmail.com>
2025-08-18 00:27:10 +0900

    [fix] MSW 활성화 환경변수 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { initializeMockServiceWorker } from '@/mocks';
 import App from '@/App.tsx';
 import '@/scss/global.scss';
 
-if (import.meta.env.DEV) {
+if (import.meta.env.DEV && import.meta.env.VITE_API_MOCKING === 'true') {
   await initializeMockServiceWorker();
 }
 
