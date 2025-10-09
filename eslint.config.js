88d9fb41392a8ea778b5737034a080dc9ee5016f
mkki <devmkki@gmail.com>
2025-10-10 01:59:47 +0900

    #98 [feat] eslint-plugin-jsx-a11y 플러그인 추가

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -19,6 +20,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -26,6 +28,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      ...jsxA11y.configs.recommended.rules,
     },
   },
   storybook.configs['flat/recommended']
