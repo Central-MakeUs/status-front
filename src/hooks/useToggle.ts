91546bd4e3a9857c98c43efeffd83b11beb14604
mkki <devmkki@gmail.com>
2025-10-11 00:14:36 +0900

    [chore] 사용하지 않는 코드 제거

--- src/hooks/useToggle.ts
+++
@@ -1,6 +0,0 @@
-import { useReducer } from 'react';
-
-export const useToggle = (initialValue: boolean = false) => {
-  const [state, toggle] = useReducer((state: boolean) => !state, initialValue);
-  return [state, toggle] as const;
-};
